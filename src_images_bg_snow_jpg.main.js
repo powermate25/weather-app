865b1595011df4ff844999d3af9eddca0b3888da
powermate <218421152+powermate25@users.noreply.github.com>
2025-12-24 19:45:06 +0000

    Deployment commit

---
+++ src_images_bg_snow_jpg.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunktodo_list_webpack"] = self["webpackChunktodo_list_webpack"] || []).push([["src_images_bg_snow_jpg"],{
+
+/***/ "./src/images/bg/snow.jpg":
+/*!********************************!*\
+  !*** ./src/images/bg/snow.jpg ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "a3480ae30e2f65f9d85c.jpg";
+
+/***/ })
+
+}]);
---
+++ src_images_icons_partly-cloudy-day_svg.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunktodo_list_webpack"] = self["webpackChunktodo_list_webpack"] || []).push([["src_images_icons_partly-cloudy-day_svg"],{
+
+/***/ "./src/images/icons/partly-cloudy-day.svg":
+/*!************************************************!*\
+  !*** ./src/images/icons/partly-cloudy-day.svg ***!
+  \************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "34335db63940b935c209.svg";
+
+/***/ })
+
+}]);
